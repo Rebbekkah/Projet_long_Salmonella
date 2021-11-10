2873e4c637e025317b964738230e33de250b2830
Rebbekkah <90262374+Rebbekkah@users.noreply.github.com>
2021-11-10 15:36:55 +0100

    Add files via upload

--- projet_long.py
+++ projet_long.py
@@ -1,7 +1,33 @@
 import os, sys
+import argparse
 
 
 
-
-def parser() :
+def get_arguments() :
     parser = argparse.ArgumentParser(description = '')
+
+    parser.add_argument('-1', 'sequence_1', dest = 'bovine', type = str, required = True, 
+                    help = "bovine sequence from scaffolding with Medusa")
+
+    parser.add_argument('-2', 'sequence_2', dest = 'poultry', type = str, required = True, 
+                    help = "poultry sequence from scaffolding with Medusa")
+
+    parser.add_argument('-ref', dest = 'reference', type = str, required = True,
+                    help = 'reference file FASTA format')
+
+
+
+#récupérer les mutations du fichier.g.vcf?
+
+def get_mutation() :
+
+
+
+def main() :
+    #path_data = 
+    args = get_arguments()
+
+
+
+if __name__ == "__main__" :
+    main()
